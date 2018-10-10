e34739f0dc5487ea2e81672dc1e0a562e96ad4af
隆一 東 <sear.azazel@gmail.com>
2018-10-10 14:03:29 +0000

    fix: 依存ライブラリの更新

--- bluemix-settings.js
+++ bluemix-settings.js
@@ -54,9 +54,9 @@ var settings = module.exports = {
     functionGlobalContext: {
     	moment : require("moment"),
     	itunesconnect : require("itunesconnectanalytics"),
-    	//wget : require("node-wget"),
+    	wget : require("node-wget"),
     	fs : require("fs"),
-    	//request : require("request")
+    	request : require("request")
 	},
 
     storageModule: require("./couchstorage")
